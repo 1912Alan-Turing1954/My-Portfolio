da6c21b4b0567e2facd74fb2087f1bd50e038872
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-02-19 15:00:53 -0800

    png add.

---
+++ Me.js
@@ -0,0 +1,6 @@
+const object = {
+    name: "me",
+    skill_Path: "Web Developer || Software Developer",
+    location: "Califofornia",
+    skills: ["JavaScript", "HTML", "css", "Node.js", "Bash", "C#", "CMD", "Git"]
+};
